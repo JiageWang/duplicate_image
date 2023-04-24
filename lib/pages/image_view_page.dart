83e766bc86c3730ad927488cc952f8a8fd9e5c3e
JiageWang <1076050774@qq.com>
2023-04-25 00:01:36 +0800

    分文件夹展示

--- lib/pages/image_view_page.dart
+++ lib/pages/image_view_page.dart
@@ -17,18 +17,7 @@ class ImageViewPage extends BasePage {
       builder: (context, snapshot) {
         Widget widget;
         if (snapshot.connectionState == ConnectionState.done) {
-          widget = ListView( // 提供滑动功能
-            children: [
-              Wrap(
-                  direction: Axis.horizontal,
-                  spacing: 10,
-                  runSpacing: 10,
-                  children: snapshot.data!
-                      .map((path) => Image.file(File(path),
-                          height: 400, fit: BoxFit.fitHeight))
-                      .toList())
-            ],
-          );
+          widget = _renderlistView(snapshot.data);
         } else {
           widget = const Text("test");
         }
@@ -37,18 +26,45 @@ class ImageViewPage extends BasePage {
     );
   }
 
-  Future<List<String>> get _imageList async {
-    List<String> imagePathList = [];
+  Future<Map<String, List<String>>> get _imageList async {
+    Map<String, List<String>> imagePathList = {};
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = '${documentsDirectory.path}${Platform.pathSeparator}';
+    String path = '${documentsDirectory.path}${Platform.pathSeparator}images';
 
-    Stream<FileSystemEntity> fileList = Directory(path).list();
+    Stream<FileSystemEntity> fileList = Directory(path).list(recursive: true);
     await for (FileSystemEntity fileSystemEntity in fileList) {
       var path = fileSystemEntity.path;
       if (FileSystemEntity.typeSync(path) == FileSystemEntityType.file &&
-          path.endsWith(".jpg")) imagePathList.add(fileSystemEntity.path);
+          path.endsWith(".jpg")) {
+        String dir = fileSystemEntity.parent.path;
+        if (imagePathList.containsKey(dir)) {
+          imagePathList[dir]!.add(path);
+        } else {
+          imagePathList[dir] = [path];
+        }
+      }
     }
     return imagePathList;
+  }
+
+  ListView _renderlistView(Map<String, List<String>>? pathMap) {
+    List<Widget> itemList = [];
+
+    pathMap?.forEach((key, value) {
+      itemList.add(ListTile(title: Text(key)));
+      itemList.add(Container(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Wrap(
+              direction: Axis.horizontal,
+              spacing: 10,
+              runSpacing: 10,
+              children: value
+                  .map((path) => Image.file(File(path),
+                      height: 200, fit: BoxFit.fitHeight))
+                  .toList())));
+    });
+
+    return ListView(children: itemList);
   }
 
   @override
